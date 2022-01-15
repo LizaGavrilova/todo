88ca5d5dc4abe5b51c622927de0beb91f5555dea
Liza <gliza.gavrilova@yandex.ru>
2022-01-15 15:44:41 +0300

    Components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-  <div>
-    <h1>My ToDo List</h1>
-    <input placeholder='search' />
-    <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
-    </ul>
-  </div>  
-);
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css';
+
+const App = () => {
+
+  const todoData = [
+    {label: 'Drink Coffee', important: false, id: 1},
+    {label: 'Make Awesome App', important: true, id: 2},
+    {label: 'Have a lunch', important: false, id: 3}
+  ];
+
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
+    </div>
+  );
+}
+
+const el = <App />;
 
 ReactDOM.render(el, document.getElementById('root'));
