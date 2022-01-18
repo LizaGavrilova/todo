d4d953745a90947da92fb80961234074bb2149d9
Liza <gliza.gavrilova@yandex.ru>
2022-01-18 17:50:02 +0300

    Состояние и события

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,30 +1,93 @@
-import React from "react";
+import React, { Component } from "react";
 
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false}) => {
-    const style = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
+class TodoListItem extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            done: false,
+            important: false
+        };
+
+        this.onlabelClick = () => {
+            this.setState((state) => {
+                return {
+                    done: !state.done
+                };
+            });
+        };
+
+        this.onMarkImportant = () => {
+            this.setState((state) => {
+                return {
+                    important: !state.important                    
+                };
+            });
+        };
     }
-    return (
-        <span className="todo-list-item">
-            <span className="todo-list-item-label"
-                style={style}>
-                { label }
+
+    render() {
+        const { label, onDeleted } = this.props;
+        const { done, important } = this.state;
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
+
+        if (important) {
+            classNames += ' important';
+        }
+        
+        return (
+            <span className={ classNames }>
+                <span className="todo-list-item-label"
+                    onClick={ this.onlabelClick }>
+                    { label }
+                </span>
+    
+                <button type="button"
+                        className="btn btn-outline-success btn-sm float-end"
+                        onClick={this.onMarkImportant}>
+                    <i className="fa fa-exclamation"></i>
+                </button>
+    
+                <button type="button"
+                        className="btn btn-outline-danger btn-sm float-end"
+                        onClick={onDeleted}>
+                    <i className="fa fa-trash-o"></i>
+                </button>
             </span>
+        );
+    }
+}
 
-            <button type="button"
-                    className="btn btn-outline-success btn-sm float-end">
-                <i className="fa fa-exclamation"></i>
-            </button>
+// const TodoListItemFunc = ({ label, important = false}) => {
+//     const style = {
+//         color: important ? 'steelblue' : 'black',
+//         fontWeight: important ? 'bold' : 'normal'
+//     }
+//     return (
+//         <span className="todo-list-item">
+//             <span className="todo-list-item-label"
+//                 style={style}>
+//                 { label }
+//             </span>
 
-            <button type="button"
-                    className="btn btn-outline-danger btn-sm float-end">
-                <i className="fa fa-trash-o"></i>
-            </button>
-        </span>
-    );
-};
+//             <button type="button"
+//                     className="btn btn-outline-success btn-sm float-end">
+//                 <i className="fa fa-exclamation"></i>
+//             </button>
+
+//             <button type="button"
+//                     className="btn btn-outline-danger btn-sm float-end">
+//                 <i className="fa fa-trash-o"></i>
+//             </button>
+//         </span>
+//     );
+// };
 
 export default TodoListItem;
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,13 +3,14 @@ import React from 'react';
 import TodoListItem from '../todo-list-item/todo-list-item';
 import './todo-list.css';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted }) => {
     const elements = todos.map( (item) => {
       return (
         <li key={item.id} className="list-group-item">
           <TodoListItem
             label={item.label}
-            important={item.important} />
+            important={item.important}
+            onDeleted={ () => onDeleted(item.id) } />
         </li>
       );
     });
